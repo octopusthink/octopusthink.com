2ddb4569d4b92b80e33166d9c8a3185cb18c7659
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-06-17 02:21:48 +0100

    feat: Add all copy from Notion

--- src/components/SiteHeader/index.jsx
+++ src/components/SiteHeader/index.jsx
@@ -2,67 +2,91 @@ import { List, Paragraph } from "@octopusthink/nautilus";
 import React, { Component } from "react";
 import { Link } from "gatsby";
 import { css } from "@emotion/core";
-import { Compass, Camera, HelpCircle, Send, Coffee } from 'react-feather';
+import { Compass, Camera, HelpCircle, Send, Coffee } from "react-feather";
+
+import config from "../../../data/SiteConfig";
 
 class SiteHeader extends Component {
   render() {
-    const { config } = this.props;
     return (
-      <header css={css`
-        display: flex;
-        justify-content: space-between;
-        font-weight: 600;
-        border-bottom: 3px solid #393F49;
-        font-family: -apple-system,BlinkMacSystemFont,San Francisco,Roboto,Segoe UI,Helvetica Neue,sans-serif;
-        font-size: 1.8rem;
-        font-weight: 500;
-        align-items: center;
-
-        a {
-          text-decoration: none;
-          border: none;
-          color: #393F49;
-          padding-bottom: 8px;
+      <header
+        css={css`
           display: flex;
-          flex-direction: column;
+          justify-content: space-between;
+          font-weight: 600;
+          border-bottom: 3px solid #393f49;
+          font-family: -apple-system, BlinkMacSystemFont, San Francisco, Roboto,
+            Segoe UI, Helvetica Neue, sans-serif;
+          font-size: 1.8rem;
+          font-weight: 500;
           align-items: center;
-          grid-gap: 4px;
 
-          &:hover {
-            color: #A42769;
+          a {
+            text-decoration: none;
+            border: none;
+            color: #393f49;
+            padding-bottom: 8px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            grid-gap: 4px;
+
+            &:hover {
+              color: #a42769;
+            }
           }
-        }
-        `}>
-        <Link to="/" css={css`
+        `}
+      >
+        <Link
+          to="/"
+          css={css`
             font-size: 2.6rem;
             align-items: start !important;
-          `}>
-          Octopus
-          <span css={css`
-            font-weight: 600;
-            `}>Think</span>
+          `}
+        >
+          {config.siteTitle}
         </Link>
 
         <nav>
-          <ul css={css`
-            list-style-type: none;
-            display: flex;
-            grid-gap: 48px;
-          `}>
+          <ul
+            css={css`
+              list-style-type: none;
+              display: flex;
+              grid-gap: 48px;
+            `}
+          >
             <li>
               <Link to="/">
-                <Compass/>
+                <Compass />
                 Home
               </Link>
             </li>
 
-            <li><Link to="/about">
-              <HelpCircle/>About</Link>
+            <li>
+              <Link to="/about">
+                <HelpCircle />
+                About
+              </Link>
             </li>
 
-            <li><Link to="/services"><Coffee/>Services</Link></li>
-            <li><Link to="/work"><Camera/>Work</Link></li>
-            <li><Link to="/contact"><Send/>Contact</Link></li>
+            <li>
+              <Link to="/services">
+                <Coffee />
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="/work">
+                <Camera />
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                <Send />
+                Contact
+              </Link>
+            </li>
           </ul>
         </nav>
       </header>
--- src/pages/work.md
+++ src/pages/work.md
@@ -1,19 +1,31 @@
 ---
-path: "/work/"
-title: "Our work"
-slug: "work"
+path: '/work/'
+title: 'Our work'
+slug: 'work'
 ---
 
 We've shipped some pretty awesome stuff over the years. Here are some things we're particularly proud of:
 
-## Firefox add-ons website
+## Firefox Add-ons
 
-We were instrumental in the re-architecting of FF Add-ons website.
+Matt was instrumental in the re-architecting of Firefox Add-ons website from a legacy Django + PHP site to a modern React site.
 
-## Gutenberg Editor for WordPress
+During the migration to WebExtensions, it was clear that the legacy Firefox Add-ons website had grown too large to maintain, so Matt worked to migrate the site to a well-tested, responsive React site with Flow, Redux, redux-saga, and server-side rendering.
 
-Led the accessibility efforts and user research on new Gutenberg components.
+The [code for the Add-ons website is available on GitHub](https://github.com/mozilla/addons-frontend).
+
+## WordPress
+
+Sarah is a full-time contributor to the WordPress project, focussing primarily on designing Gutenberg. She also lead a team of over 25 community members in exploratory user research and usability studies.
+
+Matt was the [Accessibility Lead for WordPress 5.0](https://make.wordpress.org/core/2018/10/03/a-plan-for-5-0/), one of the biggest WordPress releases in history, and worked to [improve WordPress' editing experience for _all users_](https://make.wordpress.org/core/2018/10/18/regarding-accessibility-in-gutenberg/).
+
+Matt also worked to drastically improve Gutenberg's end-to-end tests to prevent regressions and maintain a fast development pace.
 
 ## localForage
 
-blah blah offline storage
+Matt is the author and co-maintainer of localForage, used in production on sites like [WordPress.com](http://wordpress.com/), [addons.mozilla.org](http://addons.mozilla.org/), and [mobile.twitter.com](http://mobile.twitter.com/). localForage greatly simplifies offline storage for web developers and app developers who use JavaScript. We continue to invest time into the project as part of our commitment to open source.
+
+## Nebula Streaming Service
+
+Developed the entire web infrastructure for the [Nebula streaming service](https://watchnebula.com), a paid video streaming service from [Standard Broadcast creators](https://standard.tv/). Using a barebones video backend service, Matt planned, created, and ran the entire infrastructure needed for the Nebula web app and iOS app—a Django API, a server-rendered React app, Stripe integration, and a Kubernetes cluster on AWS—in under five months.
