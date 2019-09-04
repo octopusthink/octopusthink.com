248bf0ceb19c5e70ef0f938e2e3516d80896178e
sarah <sarah@triggersandsparks.com>
2019-09-04 01:46:32 -0400

    fix: Rearrange blog archive pages so they're legible.

--- src/components/PageBody/index.js
+++ src/components/PageBody/index.js
@@ -4,35 +4,36 @@ import { css } from '@emotion/core';
 import theme from '../../../config/theme';
 
 const PageBody = props => {
-  const { children } = props;
-  return (
-    <article
-      css={css`
-        background: ${theme.colors.neutral.white};
-        margin: 6.4rem auto;
-        padding: ${theme.site.mobilePadding};
-        min-height: 70vh;
+	const { children } = props;
+	return (
+		<article
+			css={css`
+				background: ${theme.colors.neutral.white};
+				margin: 6.4rem auto;
+				padding: ${theme.site.mobilePadding};
+				min-height: 70vh;
+				justify-content: center;
+				display: flex;
+				flex-direction: column;
 
-        @media screen and (min-width: 640px) {
-          padding: ${theme.site.tabletPadding};
-        }
-        @media screen and (min-width: 1024px) {
-          padding: ${theme.site.desktopPadding};
-          max-width: ${theme.site.maxSiteWidth};
-        }
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          align-items: center;
-          max-width: ${theme.site.maxContentWidth};
-        `}
-      >
-        {children}
-      </div>
-    </article>
-  );
+				@media screen and (min-width: 640px) {
+					padding: ${theme.site.tabletPadding};
+				}
+				@media screen and (min-width: 1024px) {
+					padding: ${theme.site.desktopPadding};
+					max-width: ${theme.site.maxSiteWidth};
+				}
+			`}
+		>
+			<div
+				css={css`
+					max-width: ${theme.site.maxContentWidth};
+				`}
+			>
+				{children}
+			</div>
+		</article>
+	);
 };
 
 export default PageBody;
