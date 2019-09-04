9f590ebf274ce435daf68120adae04b4d9381722
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-09-04 10:03:46 -0400

    fix: Indentation and the "Author" field

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,45 +5,45 @@
  */
 
 module.exports = {
-	siteMetadata: {
-		title: `Octopus Think`,
-		description: `Octopus Think is a design and development agency based in Scotland. We build inclusive, usable, and smart websites and apps.`,
-		author: `@octopusthink`,
-		siteUrl: `https://octopusthink.com`,
-	},
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-emotion',
-		{
-			resolve: 'gatsby-plugin-sitemap',
-			options: {
-				// Exclude specific pages or groups of pages using glob parameters
-				// See: https://github.com/isaacs/minimatch
-				exclude: ['/blog', '/blog/*', '/blog/*/*', '/portfolio/*'],
-			},
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'content',
-				path: `${__dirname}/src/content/`,
-				ignore: [`**/*.js`],
-			},
-		},
-		'gatsby-transformer-remark',
-		{
-			resolve: `gatsby-plugin-favicon`,
-			options: {
-				logo: './static/favicon.png',
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-react-svg',
-			options: {
-				rule: {
-					include: /static/, // See below to configure properly
-				},
-			},
-		},
-	],
+  siteMetadata: {
+    title: `Octopus Think`,
+    description: `Octopus Think is a design and development agency based in Scotland. We build inclusive, usable, and smart websites and apps.`,
+    author: `@octopusthinks`,
+    siteUrl: `https://octopusthink.com`,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        exclude: ['/blog', '/blog/*', '/blog/*/*', '/portfolio/*'],
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content/`,
+        ignore: [`**/*.js`],
+      },
+    },
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './static/favicon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /static/, // See below to configure properly
+        },
+      },
+    },
+  ],
 };
