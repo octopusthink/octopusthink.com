570585396481ad0c59d70caad536296d5f7b1c06
sarah <sarah@triggersandsparks.com>
2019-11-26 16:21:31 +0000

    feat: Show project details on hover.

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -18,75 +18,90 @@ const PortfolioCard = (props) => {
   const { slug, summary, title, thumbnail } = props;
 
   return (
-    <article
+    <Link
+      tabIndex="-1"
+      to={slug}
       css={css`
-        background: ${theme.colors.neutral.black} url(${thumbnail});
-        background-position: center;
-        background-size: cover;
-        display: flex;
-        flex-direction: column;
-        align-items: start;
-        justify-content: center;
-        padding: 2.4rem;
+        border-bottom: 0;
+        &:hover {
+          .details {
+            opacity: 1;
+          }
+        }
       `}
     >
-      <div
+      <article
         css={css`
-          opacity: 0;
+          background: ${theme.colors.neutral.black} url(${thumbnail});
+          background-position: center;
+          background-size: cover;
+          display: flex;
+          flex-direction: column;
+          align-items: start;
+          justify-content: center;
         `}
       >
-        <header>
-          <div
-            css={css`
-              display: none;
-              margin-bottom: 0.4rem;
-            `}
-          >
-            <Tags label="Category">
-              <Tags.Tag>#WordPress</Tags.Tag>
-            </Tags>
-          </div>
-          <Link tabIndex="-1" to={slug}>
-            <Heading
-              inverse
-              css={css`
-                margin-bottom: 1.6rem;
-              `}
-              level={2}
-            >
-              {title}
-            </Heading>
-          </Link>
-        </header>
-        <Paragraph
-          inverse
-          small
+        <div
+          className="details"
           css={css`
-            margin-bottom: 0;
+            background: ${theme.colors.neutral.black};
+            opacity: 0;
+            padding: 2.4rem;
           `}
         >
-          {summary}
-        </Paragraph>
-        <footer>
-          <Button
-            to={slug}
+          <header>
+            <div
+              css={css`
+                display: none;
+                margin-bottom: 0.4rem;
+              `}
+            >
+              <Tags label="Category">
+                <Tags.Tag>#WordPress</Tags.Tag>
+              </Tags>
+            </div>
+            <Link tabIndex="-1" to={slug}>
+              <Heading
+                inverse
+                css={css`
+                  margin-bottom: 1.6rem;
+                `}
+                level={2}
+              >
+                {title}
+              </Heading>
+            </Link>
+          </header>
+          <Paragraph
+            inverse
+            small
             css={css`
-              border: 0;
-              margin-left: 0;
-
-              &:hover {
-                box-shadow: none;
-                color: ${theme.colors.state.hoverText};
-              }
+              margin-bottom: 0;
             `}
-            navigation
-            minimal
           >
-            Continue reading<VisuallyHidden> {title}</VisuallyHidden>
-          </Button>
-        </footer>
-      </div>
-    </article>
+            {summary}
+          </Paragraph>
+          <footer>
+            <Button
+              to={slug}
+              css={css`
+                border: 0;
+                margin-left: 0;
+
+                &:hover {
+                  box-shadow: none;
+                  color: ${theme.colors.state.hoverText};
+                }
+              `}
+              navigation
+              minimal
+            >
+              Continue reading<VisuallyHidden> {title}</VisuallyHidden>
+            </Button>
+          </footer>
+        </div>
+      </article>
+    </Link>
   );
 };
 
