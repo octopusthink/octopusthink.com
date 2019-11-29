d3a6a373f3d4c1648e5f64eca1800ada9e0bc957
sarah <sarah@triggersandsparks.com>
2019-11-29 12:46:35 +0000

    feat: Add database of testimonials and show one at random.
    Fix #115.

--- src/components/TestimonialCarousel/index.js
+++ src/components/TestimonialCarousel/index.js
@@ -2,43 +2,38 @@ import { Link, Paragraph, metadata, useTheme } from '@octopusthink/nautilus';
 import React from 'react';
 import { css } from '@emotion/core';
 
+import TestimonialData from '../../../data/testimonials.yaml';
 import Divider from 'components/Divider';
 import Panel from 'components/Panel';
 import Testimonial from 'components/Testimonial';
 
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
+
 const TestimonialCarousel = (props) => {
-  const { author, children, company, position, url } = props;
   const theme = useTheme();
+
+  // Get a random testimonial
+  const rand = getRandomInt(0, TestimonialData.length);
+  let author, position, company, url;
+
+  if (TestimonialData) {
+    author = TestimonialData[rand].name;
+    position = TestimonialData[rand].title;
+    company = TestimonialData[rand].company;
+    url = TestimonialData[rand].url;
+  }
 
   return (
     <React.Fragment>
       <Divider light />
       <Panel dark>
-        <Testimonial
-          author="Dave Wiskus"
-          position="CEO"
-          company="Standard Broadcast"
-          url="https://standard.tv"
-        >
+        <Testimonial author={author} position={position} company={company} url={url}>
           <Paragraph large inverse>
-            {/* prettier-ignore */}
-            Matt built an entire{' '}
-            <Link
-              as="a"
-              href="https://watchnebula.com/"
-              css={css`
-                color: #e33592;
-              `}
-            >
-              streaming video platform
-            </Link>{' '}
-            complete with a custom API and layers upon layers of tests to validate all future
-            changes against. All of this in about six months. It was truly awe-inspiring to watch.
-          </Paragraph>
-
-          <Paragraph large inverse>
-            Do me a favor and don't hire Octopus Think, because I'd like to book all of their hours
-            myself.
+            {TestimonialData[rand].quote}
           </Paragraph>
         </Testimonial>
       </Panel>
