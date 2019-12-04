f408bad1a0d5721d2d0921c048117b10962803f4
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-12-04 20:32:44 +0000

    feat: Rearrange friends

--- src/templates/Portfolio/index.js
+++ src/templates/Portfolio/index.js
@@ -54,21 +54,21 @@ export const PortfolioList = (props) => {
         <TestimonialCarousel />
 
         <GridLayout columns="3" gap="8.0">
-          <Image src="google.png" alt="Google logo" />
+          <Image src="standard.png" alt="Standard Broadcast logo" />
           <Image src="firefox.png" alt="Firefox logo" />
+          <Image src="wordpress.com.png" alt="WordPress.com logo" />
+          <Image src="snp.png" alt="Scottish National Party logo" />
+          <Image src="ns-government.png" alt="Nova Scotia Government logo" />
+          <Image src="google.png" alt="Google logo" />
           <Image src="mozilla.png" alt="Mozilla logo" />
-          <Image src="wordpress.com.png" alt="WordPress logo" />
           <Image src="automattic.png" alt="Automattic logo" />
           <Image src="ea.png" alt="EA logo" />
           <Image src="10up.png" alt="10up logo" />
-          <Image src="fernwood.png" alt="Fernwood logo" />
+          <Image src="fernwood.png" alt="Fernwood Publishing logo" />
+          <Image src="nebula.png" alt="Nebula logo" />
           <Image src="arbor-day-foundation.png" alt="Arbor Day Foundation logo" />
           <Image src="hachette.png" alt="Hachette Book Group logo" />
-          <Image src="nebula.png" alt="Nebula logo" />
-          <Image src="ns-government.png" alt="Nova Scotia Government logo" />
           <Image src="saffron-marigold.png" alt="Saffron Marigold logo" />
-          <Image src="snp.png" alt="Scottish National Party logo" />
-          <Image src="standard.png" alt="Standard logo" />
           <Image src="wicker-emporium.png" alt="Wicker Emporium logo" />
           <Image src="wordpress.png" alt="WordPress logo" />
           <Image src="nslc.png" alt="Nova Scotia Liquor Commission logo" />
