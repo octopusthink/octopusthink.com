dedc043ca3cc6d25e17e547f786c0f26f4565e4a
sarah <sarah@triggersandsparks.com>
2020-01-13 19:58:44 +0000

    feat: Implement ability to hide image margins.

--- src/components/DecorativeImage/index.js
+++ src/components/DecorativeImage/index.js
@@ -4,13 +4,16 @@ import { css } from '@emotion/core';
 import Image from 'components/Image';
 
 const DecorativeImage = (props) => {
-  const { alt, children, image, overlap } = props;
+  const { alt, children, image, overlap, nomargin } = props;
 
   return (
     <div
       css={css`
-        margin-top: 6.4rem;
-        margin-bottom: 3.2rem;
+        ${!nomargin &&
+          css`
+            margin-top: 6.4rem;
+            margin-bottom: 3.2rem;
+          `}
 
         ${overlap &&
           css`
