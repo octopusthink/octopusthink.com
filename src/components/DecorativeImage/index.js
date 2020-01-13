71c0610f41fd016d5d2b0bf295e0db20b562dfba
sarah <sarah@triggersandsparks.com>
2020-01-13 20:13:45 +0000

    feat: Add a background option for decorative images.

--- src/components/DecorativeImage/index.js
+++ src/components/DecorativeImage/index.js
@@ -1,14 +1,22 @@
+import { useTheme } from '@octopusthink/nautilus';
 import React from 'react';
 import { css } from '@emotion/core';
 
 import Image from 'components/Image';
 
 const DecorativeImage = (props) => {
-  const { alt, children, image, overlap, nomargin } = props;
+  const { alt, background, children, image, overlap, nomargin } = props;
+  const theme = useTheme();
 
   return (
     <div
       css={css`
+        ${background &&
+          css`
+            background: ${theme.colors.neutral.grey800};
+            border: 8px solid ${theme.colors.neutral.grey800};
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.125);
+          `}
         ${!nomargin &&
           css`
             margin-top: 6.4rem;
