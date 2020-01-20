3e646b0c3b392e3210050bf19facfc8d1981526a
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-01-20 00:33:14 +0000

    feat: Add PortfolioCards component for homepage

---
+++ src/components/PortfolioCards/index.js
@@ -0,0 +1,69 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import { knuthShuffle } from 'knuth-shuffle';
+import React, { Fragment } from 'react';
+
+import PortfolioCard from 'components/PortfolioCard';
+
+const PortfolioCards = (props) => {
+  const { maxNumber, random } = props;
+
+  const portfolioQuery = useStaticQuery(graphql`
+    query {
+      portfolioItems: allMdx(
+        sort: { fields: [fields___order], order: ASC }
+        filter: { fileAbsolutePath: { regex: "//content/work/" } }
+      ) {
+        edges {
+          node {
+            fields {
+              category
+              slug
+              summary
+              summaryShort
+              title
+              titleShort
+              thumbnail
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  let itemsToUse = portfolioQuery.portfolioItems.edges.slice(0);
+  if (random) {
+    itemsToUse = knuthShuffle(itemsToUse.slice(0));
+  }
+  if (maxNumber) {
+    itemsToUse = itemsToUse.slice(0, maxNumber);
+  }
+
+  return (
+    <Fragment>
+      {itemsToUse.map(({ node }) => {
+        const { fields } = node;
+        const { category, slug, summary, summaryShort, title, titleShort, thumbnail } = fields;
+        const displayTitle = titleShort || title;
+        const displaySummary = summaryShort || summary;
+
+        return (
+          <PortfolioCard
+            key={slug}
+            category={category}
+            slug={slug}
+            summary={displaySummary}
+            title={displayTitle}
+            thumbnail={thumbnail}
+          />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+PortfolioCards.defaultProps = {
+  maxNumber: 3,
+  random: false,
+};
+
+export default PortfolioCards;
