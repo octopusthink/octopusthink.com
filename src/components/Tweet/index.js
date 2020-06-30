4fcc6098183bc18a3222b21cedab54d5022f749c
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-06-30 17:55:16 +0100

    fix: Improve Tweet loading and mobile behaviour

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -1,4 +1,5 @@
 import { css } from '@emotion/core';
+import { Link, VisuallyHidden } from '@octopusthink/nautilus';
 import { Tweet as MDXTweet } from '@pauliescanlon/gatsby-mdx-embed';
 import React from 'react';
 
@@ -8,9 +9,14 @@ const Tweet = (props) => {
     <div
       css={css`
         margin-bottom: 3.2rem;
+        min-height: 260px;
+        max-width: 90vw;
       `}
     >
       <MDXTweet align="center" hideConversation tweetLink={tweetLink} />
+      <VisuallyHidden>
+        <Link href={tweetLink}>{tweetLink}</Link>
+      </VisuallyHidden>
     </div>
   );
 };
