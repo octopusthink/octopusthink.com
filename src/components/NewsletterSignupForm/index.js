e6810c2afe78cb86efdbc52f05e00975f5be486c
sarah semark <sarah@octopusthink.com>
2020-10-25 15:52:04 +0000

    feat: Switch newsletter form to Buttondown.
    Fix #145.

--- src/components/NewsletterSignupForm/index.js
+++ src/components/NewsletterSignupForm/index.js
@@ -1,16 +1,14 @@
-import { Button, TextField, useTheme } from '@octopusthink/nautilus';
+import { Button, TextField } from '@octopusthink/nautilus';
 import React from 'react';
 import { css } from '@emotion/core';
 
 const NewsletterSignupForm = (props) => {
   const { className, emailRef } = props;
-  const theme = useTheme();
 
   return (
     <form
-      action="https://octopusthink.us4.list-manage.com/subscribe/post?u=dec5c2d889866b4c67a61ff55&amp;id=cd3b5cf599"
+      action="https://buttondown.email/api/emails/embed-subscribe/octopusthink"
       method="post"
-      name="mc-embedded-subscribe-form"
       className={className}
       noValidate
       css={css`
@@ -22,17 +20,13 @@ const NewsletterSignupForm = (props) => {
           grid-template-columns: auto auto;
           grid-gap: 0.4rem;
         }
-
-        label {
-          color: ${theme.colors.text.inverse};
-        }
       `}
     >
       <div>
         <TextField
           autocomplete="email"
           type="email"
-          name="EMAIL"
+          name="email"
           label="Email"
           ref={emailRef}
           placeholder="eg: name@yourdomain.com"
@@ -40,16 +34,8 @@ const NewsletterSignupForm = (props) => {
             margin-bottom: 0;
           `}
         />
-      </div>
-
-      <div
-        css={css`
-          position: absolute;
-          left: -5000px;
-        `}
-        aria-hidden="true"
-      >
-        <input type="text" name="b_dec5c2d889866b4c67a61ff55_cd3b5cf599" tabIndex="-1" value="" />
+        <input type="hidden" value="1" name="embed" />
+        <input type="hidden" name="tag" value="from-website, octopus-thoughts" />
       </div>
       <Button
         css={css`
